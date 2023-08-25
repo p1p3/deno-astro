6cc1af4d3c33a76ba9bdddadbc3548575425b205
Felipe Jaramillo <fjaramillo@brainshark.com>
2023-08-25 14:25:04 +0200

    remote imports

--- astro.config.js
+++ astro.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 // patched version for now.
 // import deno from "@astrojs/deno";
 import deno from "deno-astro-adapter";
+import { add } from "https://x.nest.land/ramda@0.27.0/source/index.js";
+
+console.log('This works', add(1,2));
 
 // https://astro.build/config
 export default defineConfig({
